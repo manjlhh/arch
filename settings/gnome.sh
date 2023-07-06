feb78131778000556cffca8cd7d4255ea04e889a
devrtc0 <azat.mirzaianov@gmail.com>
2023-07-06 17:35:02 +0400

    settings for DEs

---
+++ settings/gnome.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+gsettings set org.gnome.system.locale region 'en_GB.UTF-8'
+
+gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
+
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+
+gsettings set org.gnome.desktop.interface monospace-font-name 'Hack 11'
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+
+gsettings set org.gnome.desktop.media-handling autorun-never true
+gsettings set org.gnome.desktop.media-handling automount false
+
+gsettings set org.gnome.desktop.input-sources per-window true
+gsettings set org.gnome.desktop.input-sources sources [\(\'xkb\',\ \'us\'\),\ \(\'xkb\',\ \'ru\'\)]
+gsettings set org.gnome.desktop.input-sources xkb-options [\'grp:caps_toggle\']
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source []
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward []
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 25
+gsettings set org.gnome.desktop.peripherals.keyboard delay 250
+gsettings set org.gnome.desktop.peripherals.keyboard numlock-state true
+
+gsettings set org.gnome.desktop.privacy report-technical-problems false
+gsettings set org.gnome.desktop.privacy send-software-usage-stats false
+
+# Terminal F10
+gsettings set org.gnome.Terminal.Legacy.Settings menu-accelerator-enabled false
---
+++ settings/plasma.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env sh
+
+# # konsole
+# kwriteconfig5 --file $HOME/.local/share/konsole/Profile1.profile --group Appearance --key ColorScheme "Breeze"
+# kwriteconfig5 --file $HOME/.local/share/konsole/Profile1.profile --group Appearance --key Font "Hack,11,-1,7,50,0,0,0,0,0"
+# kwriteconfig5 --file $HOME/.local/share/konsole/Profile1.profile --group General --key Name "Profile1"
+# kwriteconfig5 --file $HOME/.local/share/konsole/Profile1.profile --group General --key Parent "FALLBACK/"
+# kwriteconfig5 --file $HOME/.local/share/konsole/Profile1.profile --group Scrolling --key HistorySize "5000"
+
+# kwriteconfig5 --file $HOME/.config/konsolerc --group 'Desktop Entry' --key DefaultProfile "Profile1.profile"
+# kwriteconfig5 --file $HOME/.config/konsolerc --group General --key ConfigVersion "1"
+# kwriteconfig5 --file $HOME/.config/konsolerc --group MainWindow --key MenuBar "Disabled"
+# kwriteconfig5 --file $HOME/.config/konsolerc --group MainWindow --key ToolBarsMovable "Disabled"
+
+# # screen lock
+# kwriteconfig5 --file $HOME/.config/kscreenlockerrc --group Daemon --key Autolock "false"
+
+# # mouse
+# # kwriteconfig5 --file $HOME/.config/kcminputrc --group Mouse --key cursorSize "24"
+# # kwriteconfig5 --file $HOME/.config/kcminputrc --group Mouse --key cursorTheme "Adwaita"
+
+# # keyboard
+# kwriteconfig5 --file $HOME/.config/kcminputrc --group Keyboard --key KeyboardRepeating "0"
+# kwriteconfig5 --file $HOME/.config/kcminputrc --group Keyboard --key NumLock "0"
+# kwriteconfig5 --file $HOME/.config/kcminputrc --group Keyboard --key RepeatDelay "250"
+# kwriteconfig5 --file $HOME/.config/kcminputrc --group Keyboard --key RepeatRate "50"
+
+# # shortcuts
+# kwriteconfig5 --file $HOME/.config/kglobalshortcutsrc --group "org.flameshot.Flameshot.desktop" --key Capture "Print,,Take screenshot"
+
+# # layout
+# kwriteconfig5 --file $HOME/.config/kxkbrc --group Layout --key LayoutList "us,ru"
+# kwriteconfig5 --file $HOME/.config/kxkbrc --group Layout --key LayoutLoopCount " -1"
+# kwriteconfig5 --file $HOME/.config/kxkbrc --group Layout --key Model "pc105"
+# kwriteconfig5 --file $HOME/.config/kxkbrc --group Layout --key Options ",grp:caps_toggle"
+# kwriteconfig5 --file $HOME/.config/kxkbrc --group Layout --key ResetOldOptions "true"
+# kwriteconfig5 --file $HOME/.config/kxkbrc --group Layout --key ShowFlag "false"
+# kwriteconfig5 --file $HOME/.config/kxkbrc --group Layout --key ShowLabel "true"
+# kwriteconfig5 --file $HOME/.config/kxkbrc --group Layout --key ShowLayoutIndicator "true"
+# kwriteconfig5 --file $HOME/.config/kxkbrc --group Layout --key ShowSingle "true"
+# kwriteconfig5 --file $HOME/.config/kxkbrc --group Layout --key SwitchMode "Window"
+# kwriteconfig5 --file $HOME/.config/kxkbrc --group Layout --key Use "true"
+# kwriteconfig5 --file $HOME/.config/kxkbrc --group Layout --key VariantList ","
+
+# # desktop session
+# kwriteconfig5 --file $HOME/.config/ksmserverrc --group General --key confirmLogout "false"
+# kwriteconfig5 --file $HOME/.config/ksmserverrc --group General --key loginMode "emptySession"
+# kwriteconfig5 --file $HOME/.config/ksmserverrc --group General --key shutdownType "2"
+
+# # automounter
+# kwriteconfig5 --file $HOME/.config/kded5rc --group Module-device_automounter --key autoload "false"
+
+# # titlebar buttons
+# kwriteconfig5 --file $HOME/.config/kwinrc --group org.kde.kdecoration2 --key BorderSize "Normal"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group org.kde.kdecoration2 --key ButtonsOnLeft "M"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group org.kde.kdecoration2 --key ButtonsOnRight "FIAX"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group org.kde.kdecoration2 --key ShowToolTips "false"
+
+# # night color
+# kwriteconfig5 --file $HOME/.config/kwinrc --group NightColor --key Active "true"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group NightColor --key LatitudeAuto "56.8492"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group NightColor --key LongitudeAuto "53.2319"
+
+# # compositing
+# kwriteconfig5 --file $HOME/.config/kwinrc --group Compositing --key GLCore "true"
+
+# kwriteconfig5 --file $HOME/.config/kwinrc --group Effect-Cube --key BorderActivate "9"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group Effect-Cube --key BorderActivateCylinder "9"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group Effect-Cube --key BorderActivateSphere "9"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group Effect-Cube --key TouchBorderActivate "9"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group Effect-Cube --key TouchBorderActivateCylinder "9"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group Effect-Cube --key TouchBorderActivateSphere "9"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group Effect-DesktopGrid --key BorderActivate "9"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group Effect-DesktopGrid --key TouchBorderActivate "9"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group Effect-PresentWindows --key BorderActivate "9"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group Effect-PresentWindows --key BorderActivateAll "9"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group Effect-PresentWindows --key BorderActivateClass "9"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group Effect-PresentWindows --key TouchBorderActivate "9"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group Effect-PresentWindows --key TouchBorderActivateAll "9"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group Effect-PresentWindows --key TouchBorderActivateClass "9"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group TabBox --key BorderActivate "9"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group TabBox --key BorderAlternativeActivate "9"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group TabBox --key TouchBorderActivate "9"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group TabBox --key TouchBorderAlternativeActivate "9"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group ElectricBorders --key Bottom "None"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group ElectricBorders --key BottomLeft "None"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group ElectricBorders --key BottomRight "None"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group ElectricBorders --key Left "None"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group ElectricBorders --key Right "None"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group ElectricBorders --key Top "None"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group ElectricBorders --key TopLeft "None"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group ElectricBorders --key TopRight "None"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group TouchEdges --key Bottom "None"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group TouchEdges --key Left "None"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group TouchEdges --key Right "None"
+# kwriteconfig5 --file $HOME/.config/kwinrc --group TouchEdges --key Top "None"
+
+# # locale
+# kwriteconfig5 --file $HOME/.config/plasma-localerc --group Formats --key LANG "en_GB.UTF-8"
+# kwriteconfig5 --file $HOME/.config/plasma-localerc --group Translations --key LANGUAGE "en_US:en_GB"
+
+# # power management
+# kwriteconfig5 --file $HOME/.config/powerdevilrc --group BatteryManagement --key BatteryCriticalAction "1"
+
+# # dolphin
+# kwriteconfig5 --file $HOME/.config/kiorc --group Confirmations --key ConfirmDelete "false"

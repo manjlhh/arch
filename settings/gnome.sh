6143fe859cbe83b8538c20afae5f8baab882ffe4
devrtc0 <azat.mirzaianov@gmail.com>
2023-07-06 23:07:40 +0400

    update gnome settings

--- settings/gnome.sh
+++ settings/gnome.sh
@@ -10,6 +10,8 @@ gsettings set org.gnome.desktop.screensaver lock-enabled false
 
 gsettings set org.gnome.desktop.interface monospace-font-name 'Hack 11'
 gsettings set org.gnome.desktop.interface show-battery-percentage true
+gsettings set org.gnome.desktop.interface clock-show-date true
+gsettings set org.gnome.desktop.calendar show-weekdate true
 
 gsettings set org.gnome.desktop.media-handling autorun-never true
 gsettings set org.gnome.desktop.media-handling automount false
@@ -28,3 +30,7 @@ gsettings set org.gnome.desktop.privacy send-software-usage-stats false
 
 # Terminal F10
 gsettings set org.gnome.Terminal.Legacy.Settings menu-accelerator-enabled false
+
+gsettings set org.gnome.shell.extensions.dash-to-dock disable-overview-on-startup true
+
+gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
